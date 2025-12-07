f6b871d549ba43e9c174585b37a1677c1b1ff1d0
Srujan1 <srujanyadav95@gmail.com>
2025-12-07 13:05:48 +0530

    fixed JWT_SECRET bug

--- src/middleware/authMiddleware.ts
+++ src/middleware/authMiddleware.ts
@@ -2,7 +2,7 @@
 import type { Request, Response, NextFunction } from "express";
 import * as jwt from "jsonwebtoken";
 
-const JWT_SECRET = process.env.JWT_SECRET || "defaultsecret";
+const JWT_SECRET = process.env.JWT_SECRET_KEY || "defaultsecret";
 
 export interface AuthRequest extends Request {
   user?: {
@@ -18,14 +18,15 @@ export function authMiddleware(
   next: NextFunction
 ) {
   const authHeader = req.headers.authorization;
-
+  // console.log("authHeader", authHeader);
   if (!authHeader || !authHeader.startsWith("Bearer ")) {
     return res.status(401).json({ message: "Authorization header missing" });
   }
 
   const token = authHeader.split(" ")[1] as string;
-
+  console.log("token", token);
   try {
+    console.log("VERIFY TOKEN", JWT_SECRET);
     const decoded = jwt.verify(token, JWT_SECRET) as {
       userId: number;
       email: string;
@@ -44,5 +45,6 @@ export function adminOnly(req: AuthRequest, res: Response, next: NextFunction) {
   if (!req.user || !req.user.is_admin) {
     return res.status(403).json({ message: "Admin access required" });
   }
+  console.log("He is admin \n");
   next();
 }
